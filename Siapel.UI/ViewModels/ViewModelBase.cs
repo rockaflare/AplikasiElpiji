f6c2190f05de422edc8e7250ff39d0e1c742688a
raziqin.ninetynine <raziqin.ninetynine@gmail.com>
2022-04-26 14:46:17 +0800

    Add UI

---
+++ Siapel.UI/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Siapel.UI.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
