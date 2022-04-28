cce117e3d2b08e610bd1faa1e8c3e732ac70265e
raziqin.ninetynine <raziqin.ninetynine@gmail.com>
2022-04-28 13:48:39 +0800

    add Mainmenu binding

--- Siapel.UI/ViewModels/MainMenuViewModel.cs
+++ Siapel.UI/ViewModels/MainMenuViewModel.cs
@@ -1,4 +1,7 @@
-﻿using ReactiveUI;
+﻿using Avalonia.Controls;
+using Avalonia.ReactiveUI;
+using FluentAvalonia.UI.Controls;
+using ReactiveUI;
 using Siapel.UI.Views.Pages;
 using Splat;
 using System;
@@ -10,16 +13,61 @@ using System.Threading.Tasks;
 
 namespace Siapel.UI.ViewModels
 {
-    public class MainMenuViewModel : ReactiveObject, IScreen
+    public class MainMenuViewModel : ViewModelBase
     {
         public MainMenuViewModel()
         {
-            Locator.CurrentMutable.Register(() => new HomeView(), typeof(IViewFor<HomeViewModel>));
-            SelectedMenu = ReactiveCommand.CreateFromObservable(() => Router.Navigate.Execute(new HomeViewModel(this)));
+            Categories = new List<CategoryBase>();
+
+            Categories.Add(new Category { Name = "Home", Icon = Symbol.Home });
+            Categories.Add(new Category { Name = "Item", Icon = Symbol.List });
+
+            SelectedCategory = Categories[0];
         }
 
-        public RoutingState Router { get; } = new RoutingState();
+        public List<CategoryBase> Categories { get; }
 
-        public ReactiveCommand<Unit, IRoutableViewModel> SelectedMenu { get; }
+        public object SelectedCategory
+        {
+            get => _selectedCategory;
+            set
+            {
+                this.RaiseAndSetIfChanged(ref _selectedCategory, value);
+                SetCurrentPage();
+            }
+        }
+
+        private void SetCurrentPage()
+        {
+            if (SelectedCategory is Category cat)
+            {
+                var index = Categories.IndexOf(cat) + 1;
+                var menuPage = $"Siapel.UI.Views.Pages.{cat.Name}View";
+                if (Type.GetType(menuPage) != null)
+                {
+                    var pg = Activator.CreateInstance(Type.GetType(menuPage));
+                    CurrentPage = (IControl)pg;
+                }
+            }
+        }
+
+        public IControl CurrentPage
+        {
+            get => _currentPage;
+            set => this.RaiseAndSetIfChanged(ref _currentPage, value);
+        }
+
+        
+
+        private object _selectedCategory;
+        private IControl _currentPage = new HomeView();
+        
+    }
+
+    public abstract class CategoryBase { }
+    public class Category : CategoryBase
+    {
+        public string Name { get; set; }
+        public Symbol Icon { get; set; }
     }
 }
