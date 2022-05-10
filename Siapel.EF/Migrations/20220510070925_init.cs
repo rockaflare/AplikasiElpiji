d76c342da29a86c7a224ae62fd217a5da57f0598
raziqin.ninetynine <raziqin.ninetynine@gmail.com>
2022-05-10 16:27:29 +0800

    redesign ef

---
+++ Siapel.EF/Migrations/20220510070925_init.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Siapel.EF.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pangkalan",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nama = table.Column<string>(type: "TEXT", nullable: false),
+                    Status = table.Column<string>(type: "TEXT", nullable: false),
+                    Perma = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pangkalan", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Harga",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    PangkalanId = table.Column<int>(type: "INTEGER", nullable: false),
+                    TbLimaPuluh = table.Column<int>(type: "INTEGER", nullable: false),
+                    TbDuaBelas = table.Column<int>(type: "INTEGER", nullable: false),
+                    TbLimaSetengah = table.Column<int>(type: "INTEGER", nullable: false),
+                    TanggalUbah = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Harga", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Harga_Pangkalan_PangkalanId",
+                        column: x => x.PangkalanId,
+                        principalTable: "Pangkalan",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Transaksi",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Tanggal = table.Column<DateOnly>(type: "TEXT", nullable: false),
+                    PangkalanId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Item = table.Column<string>(type: "TEXT", nullable: false),
+                    Harga = table.Column<int>(type: "INTEGER", nullable: false),
+                    JenisBayar = table.Column<string>(type: "TEXT", nullable: false),
+                    Total = table.Column<int>(type: "INTEGER", nullable: false),
+                    Status = table.Column<string>(type: "TEXT", nullable: false),
+                    TanggalLunas = table.Column<DateOnly>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Transaksi", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Transaksi_Pangkalan_PangkalanId",
+                        column: x => x.PangkalanId,
+                        principalTable: "Pangkalan",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Pangkalan",
+                columns: new[] { "Id", "Nama", "Perma", "Status" },
+                values: new object[] { 1, "UMUM", true, "Aktif" });
+
+            migrationBuilder.InsertData(
+                table: "Pangkalan",
+                columns: new[] { "Id", "Nama", "Perma", "Status" },
+                values: new object[] { 2, "WIJAYA", false, "Aktif" });
+
+            migrationBuilder.InsertData(
+                table: "Pangkalan",
+                columns: new[] { "Id", "Nama", "Perma", "Status" },
+                values: new object[] { 3, "NIHAYAH", false, "Aktif" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Harga_PangkalanId",
+                table: "Harga",
+                column: "PangkalanId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transaksi_PangkalanId",
+                table: "Transaksi",
+                column: "PangkalanId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Harga");
+
+            migrationBuilder.DropTable(
+                name: "Transaksi");
+
+            migrationBuilder.DropTable(
+                name: "Pangkalan");
+        }
+    }
+}
--- Siapel.EF/SiapelDbContext.cs
+++ Siapel.EF/SiapelDbContext.cs
@@ -15,32 +15,22 @@ namespace Siapel.EF
         {
 
         }
-
-        public DbSet<Item> Item { get; set; }
+        
         public DbSet<Pangkalan> Pangkalan { get; set; }
-        public DbSet<TipePembayaran> Pembayaran { get; set; }
-        public DbSet<Pembelian> Pembelian { get; set; }
-        public DbSet<Penjualan> Penjualan { get; set; }
-        public DbSet<Pengeluaran> Pengeluaran { get; set; }
-        public DbSet<StokGudang> StokGudang { get; set; }
-        public DbSet<StokReal> StokReal { get; set; }
-        public DbSet<TabungBocor> TabungBocor { get; set; }
-        public DbSet<TitipTabung> TitipTabung { get;set; }
+        public DbSet<Harga> Harga { get; set; }
+        public DbSet<Transaksi> Transaksi { get; set; }
+        //public DbSet<Pengeluaran> Pengeluaran { get; set; }
+        //public DbSet<StokGudang> StokGudang { get; set; }
+        //public DbSet<StokReal> StokReal { get; set; }
+        //public DbSet<TabungBocor> TabungBocor { get; set; }
+        //public DbSet<TitipTabung> TitipTabung { get;set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Item>().HasData(
-                new Item { Id = 1, Nama = "12 KG", Harga = 195000 },
-                new Item { Id = 2, Nama = "5.5 KG", Harga = 92000 },
-                new Item { Id = 3, Nama = "50 KG", Harga = 250000 });
             modelBuilder.Entity<Pangkalan>().HasData(
-                new Pangkalan { Id = 1, Nama = "Pangkalpaha", Status = "Aktif" },
-                new Pangkalan { Id = 2, Nama = "Rajinpangkalpandai", Status = "Aktif" },
-                new Pangkalan { Id = 3, Nama = "APMS", Status = "Aktif" });
-            modelBuilder.Entity<TipePembayaran>().HasData(
-                new TipePembayaran { Id = 1, Nama = "Tunai" },
-                new TipePembayaran { Id = 2, Nama = "Transfer" },
-                new TipePembayaran { Id = 3, Nama = "Invoice" });
+                new Pangkalan { Id = 1, Nama = "UMUM", Status = "Aktif", Perma = true },
+                new Pangkalan { Id = 2, Nama = "WIJAYA", Status = "Aktif", Perma = false },
+                new Pangkalan { Id = 3, Nama = "NIHAYAH", Status = "Aktif", Perma = false });
         }
     }
 }
