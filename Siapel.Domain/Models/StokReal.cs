23997cef6fa70f4966a7f6d0286911ffdc84a3fa
raziqin.ninetynine <raziqin.ninetynine@gmail.com>
2022-06-22 13:05:59 +0800

    remove unused domain models

--- Siapel.Domain/Models/StokReal.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Siapel.Domain.Models
-{
-    public class StokReal
-    {
-        public int Id { get; set; }
-        public string Item { get; set; }
-        public string Jumlah { get; set; }
-    }
-}
